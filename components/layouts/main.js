61449329bcc8e623354c2854fc8706f45f3a0b87
JohnPonciano <jhow.ponciano@hotmail.com>
2023-10-05 22:25:37 -0300

    bug fix

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,20 +15,18 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
+        <meta name="description" content="Jonathan Ponciano" />
         <meta name="author" content="Jonathan Ponciano" />
-        <meta name="author" content="craftzdog" />
+        <meta name="author" content="hakku001" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="twitter:title" content="Jonathan Ponciano" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
-        <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
+        <meta name="twitter:site" content="@hakku001" />
+        <meta name="twitter:creator" content="@hakku001" />
         <meta property="og:site_name" content="Jonathan Ponciano" />
         <meta name="og:title" content="Jonathan Ponciano" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.craftz.dog/card.png" />
         <title>Jonathan Ponciano - Homepage</title>
       </Head>
 
